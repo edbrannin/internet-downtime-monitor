56093d7e11289cc7a37131c3231ca5e32465db5d
Ed Brannin <edbrannin@gmail.com>
2015-08-21 13:31:07 -0400

    Echoing failures; waiting 60 seconds between attempts.

--- main.sh
+++ main.sh
@@ -1,4 +1,5 @@
 while true; do
     # Can't install Python packages until the net is back, so let's start by making text files.
-    ping -o google.com && (date >> OK.txt) || (date >> FAIL.txt)
+    ping -o google.com && (date >> OK.txt) || (echo -n "FAIL "; date | tee -a FAIL.txt)
+    sleep 60
 done
